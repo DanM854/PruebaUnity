d904cfc58d3ddad782ff08d729c1c13ab6487e52
Daniel <danielleonardomora.04@gmail.com>
2026-02-25 15:45:07 -0500

    coins, newinput, camera and material

---
+++ My project/Assets/_scripts/plataform/object/coins.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coins : MonoBehaviour
+{
+    public int coinValue;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerStats.score += coinValue;
+            Debug.Log(PlayerStats.score);
+            Destroy(gameObject);
+        }
+
+    }
+}
